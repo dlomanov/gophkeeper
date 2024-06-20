726ed228ab2406a28d8d836b4072642f7ae54db3
dlomanov <dlomanov@protonmail.com>
2024-06-20 14:21:01 +0000

    update builds

--- cmd/client/build.sh
+++ cmd/client/build.sh
@@ -7,17 +7,16 @@ BUILD_COMMIT=$(git rev-parse HEAD)
 LDFLAGS="-X main.buildVersion=$BUILD_VERSION -X main.buildDate=$BUILD_DATE -X main.buildCommit=$BUILD_COMMIT -s -w"
 
 # Build for Windows
-GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_windows.exe main.go
+CC=x86_64-w64-mingw32-gcc CGO_CFLAGS="-w" CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_windows.exe main.go
 echo "build client_amd64_windows.exe"
 
 # Build for macOS
-GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_macos main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_macos main.go
 echo "build client_amd64_macos"
 
-GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAGS" -o ./builds/client_arm64_macos main.go
+CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAGS" -o ./builds/client_arm64_macos main.go
 echo "build client_arm64_macos"
 
 # Build for Linux
-GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_linux main.go
+CC="gcc" CGO_CFLAGS="-Wno-error" CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./builds/client_amd64_linux main.go
 echo "build client_amd64_linux"
-
